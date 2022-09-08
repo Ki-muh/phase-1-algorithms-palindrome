02c07aa4707780ce0edc2ebed6ab8bf94c4d2129
Martin Kamau <martinkimu09@gmail.com>
2022-09-08 19:54:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    for (let j = word.length - 1; j >= 0; j--) {
+      return word[i] === word[j] ? true : false;
+    }
+  }
 }
-
 /* 
   Add your pseudocode here
+  //loop the strings
+  //match the last letter to the first
 */
 
 /*
   Add written explanation of your solution here
+  //looping the elements of the string, they are now individual elements
+  //the individual elements can now be compared using the strict logical operator
 */
 
 // You can run `node index.js` to view these console logs
